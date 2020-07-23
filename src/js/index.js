999fc9a9bf24995e502c1de6417f3f29389e9d7c
dc9111 <45150647+dc9111@users.noreply.github.com>
2020-07-23 21:52:16 +0100

    Implemented list/like view and local storage for the app

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,13 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
+import Likes from './models/Likes';
 import * as SearchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
+import * as likesView from './views/likesView';
 import { elements, renderLoader, clearLoader } from './views/base';
+
 
 
 /* Global state
@@ -76,11 +81,13 @@ const controlRecipe = async () => {
 
         //Render Recipe
         clearLoader();
-        recipeView.renderRecipe(state.recipe);
-        console.log(recipe)
+        recipeView.renderRecipe(
+            state.recipe,
+            state.likes.isLiked(id)
+            );
 
         } catch (err) {
-            alert('Error processing recipe')
+
         }
 
 
@@ -90,6 +97,67 @@ const controlRecipe = async () => {
 
 window.addEventListener('hashchange', controlRecipe);
 window.addEventListener('load', controlRecipe);
+
+//LIST Controller
+
+const controlList = () => {
+    //create a new list if there isn't one
+    if (!state.list) state.list = new List();
+
+    //add each ingredient
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    })
+}
+
+// Handle delete update list item events
+
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+
+    if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+        state.list.deleteItem(id);
+        listView.deleteItem(id);
+    } else if (e.target.matches('.shopping__count-value')) {
+        const val = parseFloat(e.target.value, 10);
+        state.list.updateCount(id, val);
+    }
+})
+
+// LIKE CONTROLLER
+
+state.likes - new Likes();
+
+const controlLike = () => {
+    if (!state.likes) state.likes = new Likes; 
+    const currentID = state.recipe.id;
+
+    if (!state.likes.isLiked(currentID)) {
+        const newLike = state.likes.addLike(
+            currentID,
+            state.recipe.title,
+            state.recipe.author,
+            state.recipe.img
+        );
+        likesView.toggleLikeBtn(true);
+
+        likesView.renderLike(newLike);
+    } else {
+        state.likes.deleteLike(currentID);
+        likesView.toggleLikeBtn(false);
+        likesView.deleteLike(currentID);
+
+    }
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+}
+
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+    state.likes.readStorage();
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+})
 
 // Handling Recipe buttons clicks
 
@@ -103,6 +171,10 @@ elements.recipe.addEventListener('click', e => {
     } else if (e.target.matches('.btn-increase, .btn-increase *')) { 
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
+    } else if (e.target.matches('.recipe__btn--add', '.recipe__btn--add *')) {
+        controlList();
+    } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+        controlLike();
     }
-    console.log(state.recipe)
+
 });
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -9,8 +9,9 @@ export default class Recipe {
         try {
             const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
             this.title = res.data.recipe.title;
-            this.img = res.data.recipe.image;
-            this.url = res.data.recipe.imageUrls;
+            this.author = res.data.recipe.publisher;
+            this.img = res.data.recipe.image_url;
+            this.url = res.data.recipe.source_url;
             this.ingredients = res.data.recipe.ingredients;
         } catch (error) {
             console.log(error);
